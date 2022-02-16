96df33451438f072aea7d5e6f598ea83c83d4f34
carlosandrefernandes <carlosandrefernandes@gmail.com>
2022-02-15 23:11:45 -0300

    fix

--- pkg/parse/container/validate.go
+++ pkg/parse/container/validate.go
@@ -1,40 +1,49 @@
 package container
 
-type bracketType int
-
 const (
-	openBracket bracketType = iota
-	closeBracket
-	notABracket
+	openCurly  rune = '{'
+	closeCurly rune = '}'
+	openBox    rune = '['
+	closeBox   rune = ']'
+	openRound  rune = '('
+	closeRound rune = ')'
 )
 
 var pairs = map[rune]rune{'{': '}', '[': ']', '(': ')'}
 
+var btMap = map[rune]func(queue []rune, char rune) ([]rune, bool){
+	openCurly:  openBracket,
+	closeCurly: closeBracket,
+	openBox:    openBracket,
+	closeBox:   closeBracket,
+	openRound:  openBracket,
+	closeRound: closeBracket,
+}
+
 func BracketCheck(phrase string) (bool, error) {
 	var queue []rune
+    var checked bool
 	for _, v := range phrase {
-		switch getBracketType(v) {
-		case openBracket:
-			queue = append(queue, pairs[v])
-		case closeBracket:
-			if 0 < len(queue) && queue[len(queue)-1] == v {
-				queue = queue[:len(queue)-1]
-			} else {
-				return false, nil
-			}
+		if checkFunc, ok := btMap[v]; ok {
+			queue, checked = checkFunc(queue, v)
+            if(!checked){
+              return false, nil
+            }
 		}
 	}
 	return len(queue) == 0, nil
 }
 
-func getBracketType(char rune) bracketType {
-	for k, v := range pairs {
-		switch char {
-		case k:
-			return openBracket
-		case v:
-			return closeBracket
-		}
+func openBracket(queue []rune, char rune) ([]rune, bool) {
+	queue = append(queue, pairs[char])
+	return queue, true
+}
+
+func closeBracket(queue []rune, char rune) ([]rune, bool) {
+	if 0 < len(queue) && queue[len(queue)-1] == char {
+		queue = queue[:len(queue)-1]
+	} else {
+		return queue, false
 	}
-	return notABracket
+	return queue, true
 }
